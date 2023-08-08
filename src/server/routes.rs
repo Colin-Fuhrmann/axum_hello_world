68c5c67568ca6237d62699931a08b86d3a3a8e69
Colin Fuhrmann <111007734+Colin-Fuhrmann@users.noreply.github.com>
2023-08-08 15:52:43 +0200

    completed path variable lesson

--- src/server/routes.rs
+++ src/server/routes.rs
@@ -1,4 +1,4 @@
-use axum::{Router, routing::{get, post}, body::Body, Json};
+use axum::{Router, routing::{get, post}, body::Body, Json, extract::Path};
 use serde::{Serialize, Deserialize};
 
 
@@ -8,6 +8,9 @@ pub fn create_routes() -> Router<() ,Body> {
         .route("/hello_world",get(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variables/:id", get(path_variables))
+        .route("/path_variables/15", get(hard_coded_path))
+
 }
 
 
@@ -37,4 +40,14 @@ struct MirrorJson {
 pub struct MirrorJsonResponse {
     message: String,
     message_from_server: String
+}
+
+
+pub async fn path_variables(Path(id): Path<i32>) -> String {
+    return id.to_string()
+}
+
+
+pub async fn hard_coded_path() -> String {
+    return "You got 15".to_string()
 }
